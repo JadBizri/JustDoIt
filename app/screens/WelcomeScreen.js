51120d0d3bbe921dc6ee0c82f0cf6ccf18f6d8a5
Jad Bizri <91910175+JadBizri@users.noreply.github.com>
2024-09-25 14:52:37 -0400

    Welcome screen outlined

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,16 +1,50 @@
 import React from 'react';
-import { ImageBackground, StyleSheet } from 'react-native';
+import { Image, ImageBackground, StyleSheet, Text, View } from 'react-native';
 
 function WelcomeScreen(props) {
     return (
-        <ImageBackground style={styles.background} source={require('../assets/background.jpg')}></ImageBackground>
+        <ImageBackground style={ styles.background } source={require('../assets/background.jpg')}>
+            <View style={styles.logoContainer}>
+                <Image style={styles.logo} source={require('../assets/logo.png')} />
+                <Text style={styles.text}>Sell What You Don't Need</Text>
+            </View>
+
+            <View style={ styles.loginButton }></View>
+            <View style={ styles.registerButton }></View>
+        </ImageBackground>
     );
 }
 
 const styles = StyleSheet.create({
     background: {
         flex: 1,
-    }
-})
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+    },
+    loginButton: {
+        width: '100%',
+        height: 120,
+        backgroundColor: '#4baaff',
+        borderWidth: 2,
+    },
+    logo: {
+        width: 200,
+        height: 200,
+        borderWidth: 5,
+        borderRadius: 100,
+    },
+    logoContainer: {
+        position: 'absolute',
+        top: 100,
+        alignItems: 'center',
+    },
+    registerButton: {
+        width: '100%',
+        height: 120,
+        backgroundColor: '#fd64b3',
+        borderWidth: 2,
+        borderTopWidth: 0,
+    },
+});
 
 export default WelcomeScreen;
